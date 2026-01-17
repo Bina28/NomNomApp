76ff760805d2b239961bd84f17be7a445c515246
Bina <dyakovabina@gmail.com>
2026-01-17 15:49:25 +0100

    feat: add routes for SignUp and UserPage components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,6 +5,8 @@ import RecipeDetail from "./components/RecipeDetail";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import Login from "./components/Login";
+import SignUp from "./components/SignUp";
+import UserPage from "./components/UserPage";
 
 function App() {
   return (
@@ -14,6 +16,9 @@ function App() {
         <Route path="/recipes" element={<Recipes />} />
         <Route path="/recipe/:id" element={<RecipeDetail />} />
         <Route path="/login" element={<Login />} />
+       <Route path="/signup" element={<SignUp />} />
+         <Route path="/user" element={<UserPage />} />
+
       </Routes>
     </BrowserRouter>
   );
